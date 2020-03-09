9873627f4c42cf3706cffb12f97e94365746cae1
usher.james <usher.james@mssm.org>
2020-03-09 10:35:42 -0400

    added clickabilty

---
+++ docs/tileset.js
@@ -0,0 +1,11 @@
+
+
+
+function onload() {
+    var firstRow=document.getElementById("gameboard").rows[0];
+    var x=firstRow.innerCell(-1);
+    x.innerHTML="<img src='forest.png' alt='oops'/>";
+}
+
+onload();
+
